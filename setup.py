e877eae65af05d787f1dc0dc6824b337c9ba8a64
Nayan Saxena <nayan.saxena18@gmail.com>
2023-05-12 05:50:27 -0400

    Remove xformers installation since package not used

--- setup.py
+++ setup.py
@@ -22,19 +22,4 @@ def install_dependencies():
             subprocess.check_call(
                 [sys.executable, "-m", "pip", "install", package])
 
-    # Additional installation for xformers based on GPU
-    gpu_info = getoutput('nvidia-smi')
-    if 'T4' in gpu_info and not is_installed('xformers'):
-        install_with_url(
-            "https://github.com/daswer123/stable-diffusion-colab/raw/main/xformers%20prebuild/T4/python39/xformers-0.0.14.dev0-cp39-cp39-linux_x86_64.whl")
-    elif 'P100' in gpu_info and not is_installed('xformers'):
-        install_with_url(
-            "https://github.com/TheLastBen/fast-stable-diffusion/raw/main/precompiled/P100/xformers-0.0.13.dev0-py3-none-any.whl")
-    elif 'V100' in gpu_info and not is_installed('xformers'):
-        install_with_url(
-            "https://github.com/TheLastBen/fast-stable-diffusion/raw/main/precompiled/V100/xformers-0.0.13.dev0-py3-none-any.whl")
-    elif 'A100' in gpu_info and not is_installed('xformers'):
-        install_with_url(
-            "https://github.com/TheLastBen/fast-stable-diffusion/raw/main/precompiled/A100/xformers-0.0.13.dev0-py3-none-any.whl")
-
     print("Dependency check passed!")
